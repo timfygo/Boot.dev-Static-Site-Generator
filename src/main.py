eda841016d79b2917cff6ccba13ab88390413c6c
timfygo <tim@campbell.sydney>
2026-09-13 21:16:55 +1000

    smth funny

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from gen_page import generate_pages_recursive
 from copystatic import copy_files_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 
 
 def main() -> None:
@@ -14,11 +14,11 @@ def main() -> None:
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_docs)
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
